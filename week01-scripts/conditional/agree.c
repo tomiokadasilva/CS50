38c2f91b635477875c72ede2d94ebf6b2a5ae30c
lucas <lucastomioka1910@gmail.com>
2023-07-13 21:27:59 -0300

    more scripts

--- week01-scripts/conditional/agree.c
+++ week01-scripts/conditional/agree.c
@@ -4,10 +4,12 @@
 int main(void)
 {
   char c = get_char("Do you agree? ");
+  
   if (c == 'y' || c == 'Y') 
   {
     printf("Agreed. \n");
   }
+
   else if (c == 'n' || c == 'N') 
   {
     printf("Not Agreed\n");
