3075ca289a496c450759de324d928d2cd739e4b7
lucas <lucastomioka1910@gmail.com>
2024-03-25 21:10:50 -0300

    update

---
+++ week02/lecture/cla/greet02/greet02.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (int argc, string argv[]) 
+{   
+    if (argc == 2)
+    {
+        printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("Hello, World! \n");
+    }
+    
+    
+}
