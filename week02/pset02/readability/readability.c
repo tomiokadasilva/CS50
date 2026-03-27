09a81159b046fbd10eb4f5141d237dd624110352
Lucas Tomioka <lucastomioka1910@gmail.com>
2026-03-26 23:09:47 -0300

    feat: Week02 readability part2

--- week02/pset02/readability/readability.c
+++ week02/pset02/readability/readability.c
@@ -4,55 +4,85 @@
 #include <stdio.h>
 #include <string.h>
 
-int count_letters(string t);
-int count_words(string t);
-int count_sentences(string t);
+#define N 3
 
-int letter = 0;
-int words = 0;
-int sentences = 0;
+void evaluate_text(string t, int p[]);
+float coleman_liau_index(int p[]);
+void print_grade(float i);
 
-int main (void)
+int main(void)
 {
+    int properties[N] = {0, 0, 0}; // initialize array with letters, words and sentences
+                                   // respectively
+
     // prompt the user for input
     string text = get_string("Text: ");
     
     // count the numbers of letters, words and sentences in the text
-    count_letters(text);
-    count_words(text);
-    count_sentences(text);
+    evaluate_text(text, properties);
 
     // compute the coleman-liau index
-    coleman_liau_index();
+    float clindex = coleman_liau_index(properties);
 
     // print the grade level
-    print_grade();
+    print_grade(clindex);
 }
 
-int count_letters(string t)
+void evaluate_text(string t, int p[])
 {
+    if (isalpha(t[0])) // count for first letter of a word in a sentence
+    {
+        p[1]++;
+    }
+    
     for (int i = 0, n = strlen(t); i < n; i++)
     {
-        if (isalpha(t[i])) // isalpha is a function from the ctype library that verifies if a character is from the alphabet. 
-                           // you could compare if the character is between the intervals a-z || A-Z in the ASCII table as well.
+        if (isalpha(t[i])) // isalpha is a function from the ctype library that verifies if
+                           // a character is from the alphabet.you could compare if the character is 
+                           // between the intervals a-z || A-Z in the ASCII table as well.
         {
-            letter++;
-        } 
+            p[0]++;
+        }
+        
+        if (t[i] == 32) // count for each space in a sentence
+        {
+            p[1]++;
+        }
+
+        if (t[i] == '!' || t[i] == '?' || t[i] == '.') // count for sentence terminations
+        {
+            p[2]++;
+        }
     }
-    printf("Total number of letters: %i \n ", letter);
-    return letter;
 }
 
-int count_words(string t)
+float coleman_liau_index(int p[])
 {
-  // TODO
-  return 0;
+    float clindex = 0.0;
+    float L = ((float) p[0] / (float) p[1]) * 100; // cast to float
+    float S = ((float) p[2] / (float) p[1]) * 100; //
+    clindex = 0.0588 * L - 0.296 * S - 15.8;
+    return clindex;
 }
 
-int count_sentences(string t)
+void print_grade(float i)
 {
-    // TODO
-    return 0;
+    i = round(i);
+    
+    if (i < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (i > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (i >= 1 && i <= 16)
+    {
+        printf("Grade %i\n", (int) i);
+    }
+    else
+    {
+        printf("index is: %f\n", i);
+    }
 }
-
-
